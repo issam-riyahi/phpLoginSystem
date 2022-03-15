166fca6e30d9a44c26e7e400cc6108f8b502137e
RIYAHI ISSAM-EDDINE <issamriyahi2@gmail.com>
2022-03-15 16:23:30 +0100

    add javascript for the inputs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let email = document.getElementById('email');
 let uid = document.getElementById('uid');
 
 let password = document.getElementById('password');
+let r_password = document.getElementById('rpsw');
 
 if(document.getElementById('login') !== null){
 
@@ -19,10 +20,34 @@ if(document.getElementById('login') !== null){
 if(document.getElementById('signup') !== null){
 
     document.getElementById('signup').addEventListener('click', (e)=>{
-        if(uid.value === '' || name.value === '' || email.value === '' || password.value === ''){
+        if(uid.value === '' || name.value === '' || email.value === '' || password.value === '' || r_password.value === ''){
             e.preventDefault();
+
+            // console.log('heelo');
+            document.querySelectorAll('input').forEach(input => {
+                input.style.color = 'red';
+            })
             
         }
         
+        
     })
 }
+
+uid.addEventListener('keydown', (event)=> {
+    console.log(uid.value.length);
+    
+    if(event.key === 'Backspace' && uid.value.length <= 1){
+       
+        uid.style.color = '';
+        return
+       
+    }
+    if(uid.value.match(/^[a-zA-Z0-9]+$/g)){
+        uid.style.color = 'green';
+    }
+    else {
+        
+        uid.style.color = 'red';
+    }
+})
